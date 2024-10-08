c5a431530cb4b2978ddc4316b16d624090295446
Juankho <desarrollo6@aveonline.co>
2024-10-08 08:56:44 -0500

    agregando campos a la base de datos

---
+++ database/migrations/2024_10_08_134448_create_games_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('lotery_id')->constrained();
+            $table->date('game_date');
+            $table->boolean('status')->default(1);
+            $table->float('total_prize');
+            $table->foreignId('winner_id')->nullable()->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
