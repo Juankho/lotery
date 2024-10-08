977aaf3c3a27621b68fc35b78fea8096bc564328
kevinsotoguerrero <kssotog@unal.edu.co>
2024-10-08 12:55:21 -0500

    feat: CRUD lotery

--- app/Http/Requests/LoteryFilters.php
+++ app/Http/Requests/LoteryFilters.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class UpdateLoteryRequest extends FormRequest
+class LoteryFilters extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,10 +22,8 @@ class UpdateLoteryRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'sometimes|required|string|max:255',
-            'description' => 'nullable|string',
-            'status' => 'required|boolean',
-            'rules' => 'nullable|string',
+            'search' => 'string',
+            'status' => 'int'
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\LoteryController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -30,4 +31,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('user', [AuthController::class, 'user']);
 
     Route::apiResource('users', UserController::class)->only(['index']);
+    Route::get('lotery', [LoteryController::class, 'index']);
+    Route::post('lotery', [LoteryController::class, 'store']);
+    Route::patch('lotery/{lotery}', [LoteryController::class, 'update']);
 });
