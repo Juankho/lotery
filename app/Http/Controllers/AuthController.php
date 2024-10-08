1d43596d2506eff013a5ed34ccf29f91410d912f
Juankho <desarrollo6@aveonline.co>
2024-10-08 08:32:07 -0500

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    /**
+     * Register a new user.
+     *
+     * @param StoreUserRequest $request
+     *
+     * @unauthenticated
+     *
+     * @return \Illuminate\Http\JsonResponse
+     *
+     */
+    public function register(StoreUserRequest $request) : JsonResponse
+    {
+        $user = User::create($request->validated());
+
+        $user['token'] = $user->createToken($request->device_name ?? 'auth_api')->plainTextToken;
+
+        return response()->json([
+            'success' => true,
+            'message' => 'User created successfully',
+            'data' => new UserResource($user),
+        ], Response::HTTP_CREATED);
+    }
+
+    /**
+     * Authenticate the user.
+     *
+     * @unauthenticated
+     */
+    public function login(LoginUserRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $user['token'] = $user->createToken($request->device_name ?? 'auth_api')->plainTextToken;
+
+        return new UserResource($user);
+    }
+
+
+    /**
+     * Get the authenticated user.
+     */
+    public function user(Request $request)
+    {
+        return new UserResource($request->user());
+    }
+
+    /**
+     * Log the user out (Invalidate the token).
+     *
+    */
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->noContent();
+    }
+
+
+    /**
+     * Log the user out from all devices (Invalidate all tokens).
+     *
+     * This method will delete all tokens associated with the user.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    public function logoutAll(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->noContent();
+    }
+}
---
+++ app/Providers/ScrambleServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\User;
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+
+class ScrambleServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        Scramble::afterOpenApiGenerated(function (OpenApi $openApi) {
+            $openApi->secure(
+                SecurityScheme::http('bearer')
+            );
+        });
+    }
+}
