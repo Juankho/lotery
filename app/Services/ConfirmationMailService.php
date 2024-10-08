e8060bc18f372adfe9919398a89ad0c7da56a0da
kevinsotoguerrero <kssotog@unal.edu.co>
2024-10-08 10:15:59 -0500

    feat: mail

---
+++ app/Services/ConfirmationMailService.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services;
+
+use App\Mail\ConfirmationEmail;
+use Illuminate\Support\Facades\Mail;
+
+class ConfirmationMailService
+{
+    public function index(string $email, string $userName, string $link)
+    {
+        print_r(Mail::to($email)->send(new ConfirmationEmail($userName, $link)));
+
+        return 'Correo enviado con éxito';
+    }
+}
---
+++ resources/views/emails/confirmation.blade.php
@@ -0,0 +1,2 @@
+<h1>Hola, {{ $name }}</h1>
+<p>Bienvenido a Lotery. Se requiere la confirmacion de tu correo, da click en el siguiente enlace: {{ $link }}</p>
